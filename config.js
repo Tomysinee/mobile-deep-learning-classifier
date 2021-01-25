cefe6944d83f14ba8ccc3622bc6b3b5a20eb5e6f
Tomysinee <49398695+Tomysinee@users.noreply.github.com>
2021-01-24 22:19:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-icconst AppConfig = 
+const AppConfig = 
 
 {
     title : "Cervix Type Classification?"
@@ -9,7 +9,7 @@ const description = `
 This mobile app was developed by 
 - [Oluwatomisin Aina](https://npatta01.github.io)
 
-This app lets you submit a photo of cervix and it predicts the cervix type class  🍕
+This app lets you submit a photo of cervix and it predicts the cervix type class 
 The model was developed using the [food-101 dataset](https://www.vision.ee.ethz.ch/datasets_extra/food-101/) and the [fastai](https://github.com/fastai/fastai) deep learning library, which is built on PyTorch.
 To learn more about deep learning, consider taking the [fast.ai course](https://www.fast.ai/) taught by [Jeremy Howard](https://www.fast.ai/about/#jeremy) and [Rachel Thomas](https://www.fast.ai/about/#rachel).
 `;
